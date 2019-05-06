6ee073f7a30e41fc32259014e365049f0f6f713b
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2019-05-06 14:17:52 +0900

    Refactor test

--- test/basic_features_test.rb
+++ test/basic_features_test.rb
@@ -2,110 +2,67 @@ require 'test_helper'
 
 class BasicFeaturesTest < Minitest::Test
   def test_calc_add
-    io = StringIO.new
+    io = WhitespaceIO.new
 
-    push_num io, 3
-    push_num io, 8
-    add io
-    write_num io
-    exit io
-    eof io
+    io.push_num 3
+    io.push_num 8
+    io.add
+    io.write_num
+    io.exit
+    io.eof
 
     assert_ws "11", io
   end
 
   def test_calc_sub
-    io = StringIO.new
+    io = WhitespaceIO.new
 
-    push_num io, 3
-    push_num io, 8
-    sub io
-    write_num io
-    exit io
-    eof io
+    io.push_num 3
+    io.push_num 8
+    io.sub
+    io.write_num
+    io.exit
+    io.eof
 
     assert_ws "-5", io
   end
 
   def test_calc_multi
-    io = StringIO.new
+    io = WhitespaceIO.new
 
-    push_num io, 3
-    push_num io, 8
-    multi io
-    write_num io
-    exit io
-    eof io
+    io.push_num 3
+    io.push_num 8
+    io.multi
+    io.write_num
+    io.exit
+    io.eof
 
     assert_ws "24", io
   end
 
   def test_calc_div
-    io = StringIO.new
+    io = WhitespaceIO.new
 
-    push_num io, 10
-    push_num io, 2
-    div io
-    write_num io
-    exit io
-    eof io
+    io.push_num 10
+    io.push_num 2
+    io.div
+    io.write_num
+    io.exit
+    io.eof
 
     assert_ws "5", io
   end
 
   def test_calc_mod
-    io = StringIO.new
+    io = WhitespaceIO.new
 
-    push_num io, 11
-    push_num io, 3
-    mod io
-    write_num io
-    exit io
-    eof io
+    io.push_num 11
+    io.push_num 3
+    io.mod
+    io.write_num
+    io.exit
+    io.eof
 
     assert_ws "2", io
-  end
-
-  # helpers
-
-  def push_ch(io, ch)
-    num_to_ws(io, ch.ord)
-  end
-
-  def push_num(io, n)
-    ws = n.to_s(2).gsub('1', "\t").gsub('0', ' ')
-    io.write "   #{ws}\n"
-  end
-
-  def write_num(io)
-    io.write "\t\n \t"
-  end
-
-  def add(io)
-    io.write "\t   "
-  end
-
-  def sub(io)
-    io.write "\t  \t"
-  end
-
-  def multi(io)
-    io.write "\t  \n"
-  end
-
-  def div(io)
-    io.write "\t \t "
-  end
-
-  def mod(io)
-    io.write "\t \t\t"
-  end
-
-  def exit(io)
-    io.write("\n\n\n")
-  end
-
-  def eof(io)
-    io.write('$')
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ require 'minitest'
 require 'minitest/autorun'
 
 require 'pathname'
+require 'forwardable'
 
 require 'akaza'
 
@@ -17,3 +18,57 @@ module WhitespaceHelper
 end
 
 Minitest::Test.include WhitespaceHelper
+
+class WhitespaceIO
+  extend Forwardable
+
+  def_delegators :@io, :rewind, :read
+
+  def initialize
+    @io = StringIO.new
+  end
+
+  def push_ch(ch)
+    num_to_ws(io, ch.ord)
+  end
+
+  def push_num(n)
+    ws = n.to_s(2).gsub('1', "\t").gsub('0', ' ')
+    io.write "   #{ws}\n"
+  end
+
+  def write_num
+    io.write "\t\n \t"
+  end
+
+  def add
+    io.write "\t   "
+  end
+
+  def sub
+    io.write "\t  \t"
+  end
+
+  def multi
+    io.write "\t  \n"
+  end
+
+  def div
+    io.write "\t \t "
+  end
+
+  def mod
+    io.write "\t \t\t"
+  end
+
+  def exit
+    io.write("\n\n\n")
+  end
+
+  def eof
+    io.write('$')
+  end
+
+  private
+  attr_reader :io
+end
